1f9ffbac1ed710f805a46ae029272b1fe01dc964
itzjazz777-star <itzjazz777@gmail.com>
2026-03-20 19:44:09 +0530

    Add files via upload

---
+++ Helloapp.java
@@ -0,0 +1,23 @@
+
+ public class Helloapp {
+    //uc4
+    
+
+    public static void main(String[] args) {
+
+       
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } 
+        else {
+            
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+        }
+    }
+}
+   
+
+
+
